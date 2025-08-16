2825e120fa411a50d854208add8f5784fc96ece8
Ramiro Nocelli <rjnocelli@gmail.com>
2025-08-16 16:49:46 +0200

    add kafka/redpanda configuration

---
+++ api/app/kafka.py
@@ -0,0 +1,25 @@
+import json
+from aiokafka import AIOKafkaProducer
+from config import settings
+
+class Kafka:
+    def __init__(self):
+        self.producer: AIOKafkaProducer | None = None
+    async def start(self):
+        self.producer: AIOKafkaProducer(
+            bootstrap_servers=settings.kafka_bootstrap_servers,
+            client_id=settings.app_name,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+        await self.producer.start()
+
+    async def stop(self):
+        if self.producer:
+            await self.producer.stop()
+
+    async def publish_order(self, payload: dict):
+        assert self.producer is not None
+        await self.producer.send_and_await("orders.new", payload)
+
+kafka = Kafka()
+        
